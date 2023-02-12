3b51ff07a2700b14070d14f8065d9a81b918b014
z4gon <4588601+z4gon@users.noreply.github.com>
2023-02-12 17:20:18 -0300

    bullet timer

--- Scripts/Player/PlayerGun.gd
+++ Scripts/Player/PlayerGun.gd
@@ -4,6 +4,7 @@ export (float) var BULLET_SPEED = 250.0
 
 onready var parent = get_parent()
 onready var fire_origin = $Sprite/FireOrigin
+onready var fire_bullet_timer = $FireBulletTimer
 
 var Utils = preload("res://Scripts/Utils.gd")
 var PlayerBullet = preload("res://Scenes/Player/PlayerBullet.tscn")
@@ -16,8 +17,9 @@ func point_to_mouse():
 	rotation = parent.get_local_mouse_position().angle()
 
 func fire_bullet():
-	if Input.is_action_just_pressed("fire"):
+	if Input.is_action_pressed("fire") and fire_bullet_timer.time_left == 0:
 		var bullet : PlayerBullet = Utils.instantiate(self, PlayerBullet, fire_origin.global_position)
 		bullet.velocity = Vector2.RIGHT.rotated(rotation) * BULLET_SPEED
 		bullet.velocity.x *= parent.scale.x # the sprite is flipped
 		bullet.rotation = bullet.velocity.angle()
+		fire_bullet_timer.start()
