6f678182ffeeca21414f9e21f9fb59d1e312f8cf
z4gon <4588601+z4gon@users.noreply.github.com>
2023-02-13 13:34:10 -0300

    configure and connect hit and hurt boxes

--- Scripts/Enemies/Enemy.gd
+++ Scripts/Enemies/Enemy.gd
@@ -4,3 +4,6 @@ class_name Enemy
 
 export (int) var MAX_SPEED = 15
 var linear_velocity = Vector2.ZERO
+
+func _on_HurtBox_hit(damage: float):
+	queue_free()
--- Scripts/Player/Projectile.gd
+++ Scripts/Player/Projectile.gd
@@ -9,3 +9,9 @@ func _process(delta):
 
 func _on_VisibilityNotifier2D_viewport_exited(_viewport):
 	queue_free()
+
+func _on_HitBox_body_entered(body):
+	queue_free()
+
+func _on_HitBox_area_entered(hurtBox: Area2D):
+	queue_free()
