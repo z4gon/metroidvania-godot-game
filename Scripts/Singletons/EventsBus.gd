28faa88386a3e418b46d53c8120e12052bee855a
z4gon <4588601+z4gon@users.noreply.github.com>
2023-02-16 18:12:01 -0300

    improve events bus

--- Scripts/Singletons/EventsBus.gd
+++ Scripts/Singletons/EventsBus.gd
@@ -3,16 +3,19 @@ extends Node
 # by event id
 var events_listeners : Dictionary = {}
 
-func register_listener(event: String, listener: Object, callback_name: String) -> int:
+func register_listener(event: String, listener_object: Object, callback_name: String) -> int:
 	if not events_listeners.has(event):
 		events_listeners[event] = []
 	
-	events_listeners[event].append({
-		object = listener,
-		callback_name = callback_name
-	})
+	var listener = {
+		object = listener_object,
+		callback_name = callback_name,
+		idx = events_listeners[event].size() - 1
+	}
 	
-	return events_listeners[event].size() - 1
+	events_listeners[event].append(listener)
+	
+	return listener.idx
 
 func unregister_listener(event: String, listener_idx: int):
 	if not events_listeners.has(event) or events_listeners[event].size() == 0:
@@ -26,6 +29,14 @@ func trigger(event: String, args: Dictionary):
 		push_warning("trigger: there are no registered listeners for event %s" % event)
 		return
 	
+	var idxs_to_purge = []
 	for listener in events_listeners[event]:
-		# https://docs.huihoo.com/godotengine/godot-docs/godot/classes/class_object.html#class-object-callv
-		listener.object.callv(listener.callback_name, args.values())
+		if not is_instance_valid(listener.object):
+			idxs_to_purge.append(listener.idx)
+		else:
+			# https://docs.huihoo.com/godotengine/godot-docs/godot/classes/class_object.html#class-object-callv
+			listener.object.callv(listener.callback_name, args.values())
+
+	# purge objects that got deleted
+	for idx in idxs_to_purge:
+		unregister_listener(event, idx)
